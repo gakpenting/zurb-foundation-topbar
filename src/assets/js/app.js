2d118f3de3780b47bb05de2712f0e5035b1d0242
catflip <rinoakbr@gmail.com>
2020-06-28 21:59:41 +0700

    bismillah

---
+++ src/assets/js/app.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import 'what-input';
+
+// Foundation JS relies on a global variable. In ES6, all imports are hoisted
+// to the top of the file so if we used `import` to import Foundation,
+// it would execute earlier than we have assigned the global variable.
+// This is why we have to use CommonJS require() here since it doesn't
+// have the hoisting behavior.
+window.jQuery = $;
+require('foundation-sites');
+
+// If you want to pick and choose which modules to include, comment out the above and uncomment
+// the line below
+//import './lib/foundation-explicit-pieces';
+
+$(document).foundation();
+import angular from 'angular'
+
+var app = angular.module('a', []);
+app.controller('b', function($scope) {
+    $scope.menuChange='fa-bars';
+    $scope.toggleMenu=function(){
+      if($scope.menuChange==='fa-bars'){
+        $scope.menuChange='fa-times';
+        document.querySelector('.dropdown-menu-bar').style.display='block';
+      }else{
+        $scope.menuChange='fa-bars';
+        document.querySelector('.dropdown-menu-bar').style.display='none';
+      }
+    };
+});
